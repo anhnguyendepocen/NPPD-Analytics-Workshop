d26db8626361a5a70966cd9feb1d3fd0fa095440
Susan Vanderplas <S1Vande@nppd.com>
2016-12-01 15:14:41 -0600

    R basics up to vectors

---
+++ 01.Introduction/code/R-Basics.R
@@ -0,0 +1,71 @@
+# Read in the data
+tips <- read.csv("http://srvanderplas.github.com/NPPD-Analytics-Workshop/01.Introduction/data/tips.csv")
+# Create a new variable - tipping rate
+tips$rate <- tips$tip / tips$total_bill
+
+
+# Addition and Subtraction
+2 + 5 - 1
+
+# Multiplication
+109*23452
+
+# Division
+3/7
+
+# Integer division
+7 %/% 2
+
+# Modulo operator (Remainder)
+7 %% 2
+
+# Powers
+1.5^3
+
+# Exponentiation
+exp(3)
+
+# Logarithms
+log(3)
+log(3, base = 10)
+
+# Trig functions
+sin(0)
+cos(0)
+tan(pi/4)
+
+asin(0)
+acos(1)
+atan(1)
+
+# Variables in R
+x <- 5
+R_awesomeness <- Inf
+MyAge <- 21
+
+# Computations with variables
+log(x)
+MyAge ^ 2
+
+# Variable Names
+## Can't start with a number
+1age <- 3
+
+## Case Sensitive
+Age <- 3
+age
+
+## Reserved words can't be used
+for <- 3
+
+## R has pre-existing variables and functions you can overwrite:
+# This is a VERY bad idea:
+T <- FALSE
+F <- TRUE
+
+T == FALSE
+F == TRUE
+
+rm(T, F) # Fix it!
+# using "c" as a variable name causes a lot of problems and cryptic error messages
+
