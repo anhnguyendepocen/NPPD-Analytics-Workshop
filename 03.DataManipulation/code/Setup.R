0ed63d559db45c6afb13f41389cfa1605ed91853
Susan VanderPlas (root) <srvanderplas@gmail.com>
2016-12-28 14:14:20 -0600

    Setup file

---
+++ 03.DataManipulation/code/Setup.R
@@ -0,0 +1,10 @@
+pkgs <- c("ggplot2", "dplyr", "plyr", "tidyr", "readxl", "foreign", "lubridate")
+
+install.packages(pkgs)
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(readxl)
+library(foreign)
+library(lubridate)
